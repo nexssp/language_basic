c9abfce3d27a53e8325f7cfd5d2c56680c0ea92b
Marcin Polak <mapoart@gmail.com>
2020-09-13 20:50:11 +0200

    Added MacOS file + some missing libs for linux distros

--- basic.darwin.nexss.config.js
+++ basic.darwin.nexss.config.js
@@ -1,8 +1,8 @@
 let languageConfig = Object.assign({}, require("./basic.win32.nexss.config"));
 languageConfig.builders = {
   freebasic: {
-    install: `apt install -y libtinfo5 gcc make lib{ncurses5,gpm,x11,xext,xpm,xrandr,xrender,gl1-mesa,ffi}-dev
-`,
+    install:
+      "brew install -y gcc make lib{ncurses5,gpm,x11,xext,xpm,xrandr,xrender,gl1-mesa,ffi}-dev",
     command: "fbc",
     build: function () {
       return "fbc";
@@ -11,8 +11,5 @@ languageConfig.builders = {
     help: ``,
   },
 };
-languageConfig.compilers = {};
-languageConfig.errors = require("./nexss.basic.errors");
-languageConfig.languagePackageManagers = {};
 
 module.exports = languageConfig;
