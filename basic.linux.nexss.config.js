2c648301ede2a581b0e26bee68bcc89e7b61997f
Marcin Polak <mapoart@gmail.com>
2020-10-07 21:45:56 +0200

    Added sudo

--- basic.linux.nexss.config.js
+++ basic.linux.nexss.config.js
@@ -1,8 +1,14 @@
 let languageConfig = Object.assign({}, require("./basic.win32.nexss.config"));
+
+const os = require(`${process.env.NEXSS_SRC_PATH}/node_modules/@nexssp/os/`);
+const sudo = os.sudo();
+
 languageConfig.builders = {
   freebasic: {
-    install: `apt install -y libtinfo5 gcc make lib{ncurses5,gpm,x11,xext,xpm,xrandr,xrender,gl1-mesa,ffi}-dev
-`,
+    install: `${sudo}apt install -y libtinfo5 gcc make lib{ncurses5,gpm,x11,xext,xpm,xrandr,xrender,gl1-mesa,ffi}-dev
+${sudo}apt-get install -y libc6-i386 lib32ncurses5-dev gcc-multilib g++-multilib lib32mudflap0
+wget http://frozenfox.freehostia.com/cappy/getlibs-all.deb
+${sudo}sudo dpkg -i getlibs-all.deb`,
     command: "fbc",
     build: function () {
       return "fbc";
@@ -12,7 +18,5 @@ languageConfig.builders = {
   },
 };
 languageConfig.compilers = {};
-languageConfig.errors = require("./nexss.basic.errors");
-languageConfig.languagePackageManagers = {};
 
 module.exports = languageConfig;
